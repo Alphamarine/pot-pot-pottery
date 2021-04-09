a3a76947d50ffaa1747ad4995c23d5fe44f6c37b
Gabriele Leonardi <leonardi.gabriele.lg@gmail.com>
2021-04-09 19:04:35 +0200

    Setup

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import PrismicVue from "@prismicio/vue";
+import linkResolver from "./prismic/link-resolver";
+import htmlSerializer from "./prismic/html-serializer";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(PrismicVue, {
+  endpoint: "https://potpotpottery.cdn.prismic.io/api/v2",
+  linkResolver,
+  htmlSerializer,
+});
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
